5f34bc7ec4876007e21b67a011a1c8d9eaf359ec
Forrest Almasi <forrest.almasi@gmail.com>
2016-10-03 14:11:08 -0400

    spellcheck, click event, and link disabling

--- turnOff.js
+++ turnOff.js
@@ -1,1 +1,6 @@
 document.body.contentEditable = false;
+document.body.spellcheck = true;
+var links = document.getElementsByTagName('a');
+for (var i = 0; i < links.length; i++) {
+	links[i].setAttribute('disabled', false);
+}
--- turnOn.js
+++ turnOn.js
@@ -1,2 +1,15 @@
 document.body.spellcheck = false;
 document.body.contentEditable = true;
+
+var links = document.getElementsByTagName('a');
+for (var i = 0; i < links.length; i++) {
+	links[i].setAttribute('disabled', 'disabled');
+}
+
+document.addEventListener('click', handler, true);
+
+function handler(e) {
+	if (document.body.spellcheck) return;
+	e.stopPropagation();
+	e.preventDefault();
+}
